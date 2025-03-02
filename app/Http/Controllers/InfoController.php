d140fadc41e5c6be564ed7066bb9ac374a0412a2
abiruzzaman abir <abiruzzamanabir17@gmail.com>
2025-03-02 12:23:28 +0600

    update info

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use App\Models\nomination;
 use App\Models\Theme;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class InfoController extends Controller
@@ -77,7 +78,31 @@ class InfoController extends Controller
      */
     public function update(Request $request, nomination $nomination)
     {
-        //
+        $update_date = User::findOrFail($request->email);
+        $members = [];
+        if ($request->member_name) {
+            for ($i = 0; $i < count($request->member_name); $i++) {
+                array_push($members, [
+                    'member_name' => $request->member_name[$i],
+                    'member_designation' => $request->member_designation[$i],
+                    'member_organization' => $request->member_organization[$i],
+                    'member_contact' => $request->member_contact[$i],
+                    'member_email' => $request->member_email[$i],
+                    'members' => json_encode($members),
+                ]);
+            }
+        }
+        $update_date->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'designation' => $request->designation,
+            'organization' => $request->organization,
+            'address' => $request->address,
+            'members' => json_encode($members),
+        ]);
+
+        return redirect()->route('form.index')->with('success', 'Information Updated');
     }
 
     /**

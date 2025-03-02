7c91b599a53bc4821a64a10e4ca7524f40914bcc
abiruzzaman abir <abiruzzamanabir17@gmail.com>
2025-03-02 13:42:24 +0600

    update migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('address');
             $table->longText('members');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

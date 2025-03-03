7f8c440f4f3ed8b2f7272708132bc31133862dfb
abiruzzaman abir <abiruzzamanabir17@gmail.com>
2025-03-03 10:12:28 +0600

    fix known bug

--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -170,7 +170,7 @@
                                             </label>
                                             <input list="organisations" type="text" name="organization"
                                                 class="form-control"
-                                                @if (Auth::user()->organisations) value="{{ Auth::user()->organisations }}" @endif
+                                                @if (Auth::user()->organisation) value="{{ Auth::user()->organisation }}" @endif
                                                 required>
                                             <div class="invalid-feedback text-uppercase">Enter Your Organization Name
                                             </div>

f89eeb1ff4ea819d2e01f908e4fbe628e6a29e90
abiruzzaman abir <abiruzzamanabir17@gmail.com>
2025-03-19 11:29:58 +0600

    disable inspect feature

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -108,6 +108,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.3.slim.min.js"
         integrity="sha256-ZwqZIVdD3iXNyGHbSYdsmWP//UBokj2FHAxKuSBKDSo=" crossorigin="anonymous"></script>
+        @include('kill')
 </body>
 
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -117,6 +117,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.3.slim.min.js"
         integrity="sha256-ZwqZIVdD3iXNyGHbSYdsmWP//UBokj2FHAxKuSBKDSo=" crossorigin="anonymous"></script>
+        @include('kill')
 </body>
 
 </html>
